c073f599645d0343dbb96cd485642a468c1dcdb3
hou80houzhu <hou80houzhu@vip.qq.com>
2017-09-07 10:14:17 +0800

    init

--- parser/idparser.js
+++ parser/idparser.js
@@ -1,3 +1,7 @@
 module.exports = function (id) {
-    return "function(require, exports, module){module.exports={_scopeId:" + JSON.stringify(id) + "};}";
+    if(id) {
+        return "function(require, exports, module){module.exports={_scopeId:" + JSON.stringify(id) + "};}";
+    }else{
+        return "function(require, exports, module){module.exports={_scopeId:''};}";
+    }
 };
--- parser/templateparser.js
+++ parser/templateparser.js
@@ -1,19 +1,23 @@
 module.exports=function (info) {
-    var content=info.content;
-    if(content) {
-        var minify = require('html-minifier').minify;
-        try {
-            content = minify(content, {
-                removeComments: true,
-                collapseWhitespace: true,
-                minifyJS: true,
-                minifyCSS: true
-            });
-        } catch (e) {
-            console.log(e)
+    if(info) {
+        var content = info.content;
+        if (content) {
+            var minify = require('html-minifier').minify;
+            try {
+                content = minify(content, {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                    minifyJS: true,
+                    minifyCSS: true
+                });
+            } catch (e) {
+                console.log(e)
+            }
+        } else {
+            content = "";
         }
+        return "function(require, exports, module){module.exports={template:" + (JSON.stringify(content) || '\"\"') + "};}";
     }else{
-        content="";
+        return "function(require, exports, module){module.exports={template:''};}";
     }
-    return "function(require, exports, module){module.exports={template:"+(JSON.stringify(content)||'\"\"')+"};}";
 };
